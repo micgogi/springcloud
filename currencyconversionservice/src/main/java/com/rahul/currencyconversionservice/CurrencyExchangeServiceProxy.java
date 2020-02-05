7d452068c93c1860434917f34a76dfb15ffcdc3a
micgogi <rahul.gogyani@gmail.com>
2020-02-05 22:47:44 +0530

    eureka server added

--- currencyconversionservice/src/main/java/com/rahul/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currencyconversionservice/src/main/java/com/rahul/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -1,5 +1,6 @@
 package com.rahul.currencyconversionservice;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
  */
 @FeignClient(name="currency-exchange-service")
 @RibbonClient(name="currency-exchange-service")
+@EnableDiscoveryClient
 public interface CurrencyExchangeServiceProxy {
     @GetMapping("/current-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean retrieveExchnageValue(@PathVariable("from") String from, @PathVariable("to") String to);
